5d574cdb15da37d4cd89f23ecae19498d52727d5
kbbushman <k.bushman@yahoo.com>
2021-06-17 00:02:55 -0700

    Refactor adds BaseController

--- src/controllers/MainController.ts
+++ src/controllers/MainController.ts
@@ -1,18 +1,18 @@
-export class MainController {
+import { BaseController } from './BaseController';
+
+export class MainController extends BaseController {
   public createView(): HTMLDivElement {
-    const container = document.createElement('div');
+    const title = this.createElement('h2', 'Welcome to User Manager');
 
-    const title = document.createElement('h2');
-    title.innerText = 'Welcome to User Manager';
+    const article = this.createElement(
+      'article',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aliquid nihil et, eius deserunt laboriosam. Dolores pariatur id fuga cupiditate sed tempore, repudiandae, qui vel quaerat corrupti, nihil necessitatibus aperiam esse omnis voluptatum ipsam blanditiis nostrum est velit! At illum optio deserunt dicta ipsa quas illo eius earum vero esse!'
+    );
 
-    const article = document.createElement('article');
-    article.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aliquid nihil et, eius deserunt laboriosam. Dolores pariatur id fuga cupiditate sed tempore, repudiandae, qui vel quaerat corrupti, nihil necessitatibus aperiam esse omnis voluptatum ipsam blanditiis nostrum est velit! At illum optio deserunt dicta ipsa quas illo eius earum vero esse!';
+    const button = this.createElement('button', 'Login');
 
-    const button = document.createElement('button');
-    button.innerText = 'Login';
+    this.container.append(title, article, button);
 
-    container.append(title, article, button);
-
-    return container;
+    return this.container;
   }
 }
