08772866947d68748b14f8cc38e4600da5a0be3d
kbbushman <k.bushman@yahoo.com>
2021-06-22 23:48:18 -0700

    Refactors temp login with fetch request to api

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,13 +1,24 @@
 import { SessionToken } from './../models/AuthenticationModel';
+
+const baseUrl = 'http://localhost:4000';
+const loginUrl = baseUrl + '/login';
+
 export class LoginService {
   public async login(
     username: string,
     password: string
   ): Promise<SessionToken | undefined> {
-    if (username === 'user' && password === '1234') {
-      return {
-        username: 'Some Username',
-      } as any;
+    let options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    };
+
+    const result = await fetch(loginUrl, options);
+    if (result.status === 201) {
+      return await result.json();
     } else {
       return undefined;
     }
