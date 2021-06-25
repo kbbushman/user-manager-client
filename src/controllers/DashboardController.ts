509c76e9f1a8f5fc82ad18f57a84d657f6a936de
kbbushman <k.bushman@yahoo.com>
2021-06-24 23:24:12 -0700

    Adds private generateButtons method for crud

--- src/controllers/DashboardController.ts
+++ src/controllers/DashboardController.ts
@@ -1,8 +1,10 @@
-import { SessionToken } from './../models/AuthenticationModel';
+import { SessionToken, AccessRight } from './../models/AuthenticationModel';
 import { BaseController } from './BaseController';
 
 export class DashboardController extends BaseController {
   private sessionToken: SessionToken | undefined;
+  private searchArea: HTMLInputElement | undefined;
+  private searchResultArea: HTMLDivElement | undefined;
 
   public setSessionToken(sessionToken: SessionToken) {
     this.sessionToken = sessionToken;
@@ -13,10 +15,37 @@ export class DashboardController extends BaseController {
 
     if (this.sessionToken) {
       this.createElement('label', `Welcome ${this.sessionToken.username}`);
+      this.insertBreak();
+      this.generateButtons();
     } else {
       this.createElement('label', 'You can access public features of this app');
     }
 
     return this.container;
+  }
+
+  private generateButtons() {
+    if (this.sessionToken) {
+      for (const access of this.sessionToken.accessRights) {
+        this.createElement('button', AccessRight[access], async () => {
+          this.triggerAction(access);
+        });
+      }
+
+      if (this.sessionToken.accessRights.includes(AccessRight.READ)) {
+        this.insertBreak();
+        this.createElement('label', 'Search');
+        this.searchArea = this.createElement('input');
+        this.searchResultArea = this.createElement('div');
+      }
+    }
+  }
+
+  private async triggerAction(access: AccessRight) {
+    console.log(`${access} button clicked...`);
+  }
+
+  protected insertBreak() {
+    this.createElement('br');
   }
 }
